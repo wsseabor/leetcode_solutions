efc60a2da3c8000998fb248a20bef61f98e3ac4b
Schuyler Seaborn <wsseabor@gmail.com>
2024-02-02 16:56:48 -0500

    Climbing stairs solution

--- EasySolutions/ClimbingStairs.py
+++ EasySolutions/ClimbingStairs.py
@@ -12,12 +12,39 @@ One further, as n = 4, the solution gives 1 + 1 + 1 + 1, 2 + 2, 1 + 1 + 2, 1 + 2
 for five solutions.
 
 Okay, that's great, even if the underlying pattern is a fibonacci sequence, in problems like this
-where the patterns are not so clear, how would we go about solving this? 
+where the patterns are not so clear, how would we go about solving this? One way to reduce the time
+complexity of solving this recursively is to use a dictionary to map the number of steps present 
+(n, the number of stairs present) to the number of methods used to get there (the value mapping).
+However, we would have to sum both the current and previous values of the current key index to find
+all the possible combinations of one or two steps for however many steps are in the staircase.
 
+Doing this recurisively would be relatively quick and simple for very small values of n, but as
+it grows in size, the number of recursive function calls quickly balloons and even a modest n = 20
+would take quite a long time, not even discussing n = 50 and above. This is because at each branch of
+the decision tree, there are multiple recursive function calls that have previously been calculated, 
+and others on top of that that have also previously been calculated, so doing so for each value
+even if it was previously found is very time expensive. 
 
-
+So we create a dictionary, map the known values of 0:0 and 1:1 for the smallest base cases (where 0 steps
+require 0 combos, 1 step requires one and only one combo), and loop up to the n given, summing the
+current and previous value of the keys, and storing them for later use in the method.
 """
 
 class ClimbingStairsSolution:
     def ClimbingStairs(self, n: int) -> int:
-        pass
+        
+        if n == 0:
+            return(0)
+        if n == 1:
+            return(1)
+        if n == 2:
+            return(2)
+        
+        methods_per_steps = [1, 2]
+
+        for i in range(2, n+1):
+            methods_per_steps.append(methods_per_steps[i -1] + methods_per_steps[i - 2])
+        return(methods_per_steps[n- 1])
+
+sol = ClimbingStairsSolution()
+sol.ClimbingStairs(5)
